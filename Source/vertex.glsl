9e38fdc032d5a6e1c3617eeeed098092593e012c
Spencer Karjala <spencerkarjala@gmail.com>
2021-02-19 10:30:44 -0800

    add vertex shader

---
+++ Source/vertex.glsl
@@ -0,0 +1,12 @@
+#version 330 core
+            
+in vec4 position;      
+in vec4 sourceColor;
+            
+out vec4 fragColor;
+            
+void main() {
+  gl_Position = position;
+                
+  fragColor = sourceColor;
+}
